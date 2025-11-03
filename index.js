8c24f03cb7660a5af4280eb820b65021d39cfc6c
pankajsingh9w7-commits <pankajsingh9w7@gmail.com>
2025-11-03 15:33:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("myButton").addEventListener("click", () => {
+      alert("it work");
+    });
+
+alert("Welcome to page")
+
+
+document.getElementById("myButton").addEventListener("click", () => {
+  document.getElementById("myButton").textContent = "Clicked!";
+});
+
+document.getElementById("myButton").addEventListener("click", () => {
+  document.getElementById("myButton").style.backgroundColor = "lightgreen";
+});
